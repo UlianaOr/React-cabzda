ae050e4ee1cc75212795fa23e0c50c3e51333f85
UlianaOr <Ouliana9@gmail.com>
2022-06-22 12:56:07 +0200

    Accordion

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,28 @@ import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
-        </div>
-    );
+    <div>
+        <Accordion/>
+
+
+
+
+    </div>
+    )
+}
+
+function Accordion () {
+    console.log ("Accordion rendering")
+    return (
+        <>
+        <h3>Menu</h3>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+        </>
+    )
 }
 
 export default App;
