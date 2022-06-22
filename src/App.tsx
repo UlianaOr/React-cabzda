c5ab9a0c668fe021f9aabdc71498705716a5193d
UlianaOr <Ouliana9@gmail.com>
2022-06-22 17:22:57 +0200

    componentsRating

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Accordion from './components/accordion/Accordion';
+import Rating from './components/accordion/Rating';
 
 function App() {
     return (
     <div>
         <Accordion/>
+        <Rating/>
 
 
 
---
+++ src/components/accordion/Rating.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Rating (){
+    console.log ("Rating rendering")
+    return (
+        <>
+        <Star />
+        <Star />
+        <Star />
+        <Star />
+        <Star />
+        </>
+    )
+}
+
+
+
+
+
+
+export default Rating
