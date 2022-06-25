f5217a734ced401255ea893afa1ced93c3cc675f
UlianaOr <Ouliana9@gmail.com>
2022-06-25 14:01:42 +0200

    AppTitle

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Accordion from './components/accordion/Accordion';
+import AppTitle from './components/accordion/accordion/AppTitle';
 import Rating from './components/accordion/Rating';
 import Star from './components/accordion/Star';
 
 function App() {
     return (
     <div>
+        <AppTitle/>
         <Accordion/>
         <Rating/>
         <Star/>
---
+++ src/components/accordion/accordion/AppTitle.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function AppTitle () {
+    console.log ("AppTitle rendering")
+    return <>This is App component</>
+}
+
+
+export default AppTitle
