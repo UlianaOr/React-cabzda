c3e168ee0ccfab06001122e1b7d9896a12f2d9b2
UlianaOr <Ouliana9@gmail.com>
2022-08-17 16:30:58 +0200

    Ono

---
+++ src/components/accordion/accordion/OnOff.tsx
@@ -0,0 +1,17 @@
+import { type } from "os";
+import React from 'react';
+
+
+type PropsType = { 
+    on: boolean
+}
+
+function OnOff (props: PropsType) {
+    return <div>
+
+    </div>
+
+}
+
+
+export default OnOff;
