3c8d3a35c2732b83e56fa8349f2878c909e8bc82
UlianaOr <Ouliana9@gmail.com>
2022-08-17 16:36:59 +0200

    Style

--- src/components/accordion/accordion/OnOff.tsx
+++ src/components/accordion/accordion/OnOff.tsx
@@ -6,8 +6,21 @@ type PropsType = {
     on: boolean
 }
 
+const onStyle = {};
+const offStyle = {};
+const indicatorStyle = {
+    widht: "30px",
+    height: "30px",
+    borderRadius: "15px",
+    border: "1px solid black"
+};
+
+
 function OnOff (props: PropsType) {
     return <div>
+        <div></div>
+        <div></div>
+        <div style = {indicatorStyle}></div>
 
     </div>
 
