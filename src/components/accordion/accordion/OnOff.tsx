c7cda404740b30fb96e4d5c89fe25ed01bab8c7e
UlianaOr <Ouliana9@gmail.com>
2022-08-24 16:06:14 +0200

    commit

--- src/components/accordion/accordion/OnOff.tsx
+++ src/components/accordion/accordion/OnOff.tsx
@@ -1,51 +1,51 @@
 
-import React from 'react';
+// import React from 'react';
 
 
-type PropsType = { 
-    on: boolean
-}
+// type PropsType = { 
+//     on: boolean
+// }
 
- function OnOff (props: PropsType) {
+//  function OnOff (props: PropsType) {
 
-const onStyle = {
-    widht: "30px",
-    height: "20px",
-    display: "inline-block",
-    border: "1px solid black",
-    padding: "2px",
-    backgroundColor: "green" 
-};
-const offStyle = {
-    widht: "30px",
-    height: "20px",
-    display: "inline-block",
-    border: "1px solid black",
-    marginLeft: "5px",
-    padding: "2px",
-    backgroundColor: "red"
-};
-const indicatorStyle = {
-    widht: "10px",
-    height: "10px",
-    borderRadius: "5px",
-    border: "1px solid black",
-    display: "inline-block",
-    marginLeft: "5px",
-    padding: "2px",
-    backgroundColor: "blue"
-};
+// const onStyle = {
+//     widht: "30px",
+//     height: "20px",
+//     display: "inline-block",
+//     border: "1px solid black",
+//     padding: "2px",
+//     backgroundColor: "green" 
+// };
+// const offStyle = {
+//     widht: "30px",
+//     height: "20px",
+//     display: "inline-block",
+//     border: "1px solid black",
+//     marginLeft: "5px",
+//     padding: "2px",
+//     backgroundColor: "red"
+// };
+// const indicatorStyle = {
+//     widht: "10px",
+//     height: "10px",
+//     borderRadius: "5px",
+//     border: "1px solid black",
+//     display: "inline-block",
+//     marginLeft: "5px",
+//     padding: "2px",
+//     backgroundColor: "blue"
+// };
 
 
  
-     return <div>
-         <div style={onStyle}>On</div>
-         <div style={offStyle}>Off</div>
-         <div style = {indicatorStyle}></div>
+//      return <div>
+//          <div style={onStyle}>On</div>
+//          <div style={offStyle}>Off</div>
+//          <div style = {indicatorStyle}></div>
 
-     </div>
+//      </div>
 
  
-}
+// }
 
-export default OnOff;
+// export default OnOff;
